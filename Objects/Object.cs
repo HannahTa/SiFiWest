c1eadd11f39f79561fd076eeb1427df32515648d
Joshua Petherick <peth@live.co.uk>
2017-12-03 12:17:34 +0000

    First commit containing project work before Repository creation

---
+++ Objects/Object.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace Game2.Objects
+{
+    public abstract class Object
+    {
+        public abstract void draw(SpriteBatch spriteBatch);
+    }
+}
---
+++ Objects/Road.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Game2.Objects
+{
+    class Road : Object
+    {
+        // Base Properties
+        private Texture2D texture;
+        private int x;
+        private int y;
+        private int width;
+        private int height;
+
+        public Road(int x, int y, int width, int height)
+        {
+            texture = TextureManager.zoneRoadTexture;
+            this.x = x;
+            this.y = y;
+            this.height = height;
+            this.width = width;
+        }
+
+        public int getX()
+        {
+            return x;
+        }
+
+        public int getY()
+        {
+            return y;
+        }
+
+        public int getHeight()
+        {
+            return height;
+        }
+
+        public int getWidth()
+        {
+            return width;
+        }
+
+
+        public override void draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, new Rectangle(x, y, width, height), Color.GhostWhite);
+        }
+    }
+}
